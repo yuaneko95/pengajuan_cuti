1da2d1d1069c0766375fbbe2d879598be34c616e
yuaneko95 <yuaneko95@gmail.com>
2017-01-31 14:05:46 +0700

    perubahan login

--- login.php
+++ login.php
@@ -14,7 +14,39 @@
 			<div class="login-panel panel panel-default">
 				<div class="panel-heading">Log in</div>
 				<div class="panel-body">
-					<form role="form" action="proses/login_user.php" method="POST">
+				<?php  
+					session_start();
+					include 'admin/koneksi.php';
+					if (isset($_POST['login'])) {
+						$username = $_POST['username'];
+						$password = $_POST['password'];
+						$status_pegawai = $_POST['status_pegawai']; 
+						
+					$query = mysqli_query($conn, "SELECT * FROM pegawai WHERE username='$username' AND password='$password'");
+					var_dump($query);
+					if(mysqli_num_rows($query) == 0){
+						echo '<div class="alert alert-danger">Upss...!!! Login gagal.</div>';
+						} else {
+							$row = mysqli_fetch_assoc($query);
+
+							if ($row['status_pegawai'] == admin && $status_pegawai == admin) {
+								$_SESSION['username'] =  $username;
+								$_SESSION['id_pegawai'] = $row['id_pegawai'];
+								$_SESSION['status_pegawai'] = 'admin';
+								header('location:admin/index.php');
+							} elseif ($row['status_pegawai'] == pegawai && $status_pegawai == pegawai) {
+								$_SESSION['username'] =  $username;
+								$_SESSION['id_pegawai'] = $row['id_pegawai'];
+								$_SESSION['status_pegawai'] = 'pegawai';
+								header('location:index.php');
+							} else {
+								echo '<div class="alert alert-danger">Upss...!!! sorry</div>';
+							}
+						}
+					}
+					
+				?>
+					<form role="form" action="" method="POST">
 						<fieldset>
 							<div class="form-group">
 								<input class="form-control" placeholder="Username" name="username" type="text" autofocus="">
@@ -22,7 +54,14 @@
 							<div class="form-group">
 								<input class="form-control" placeholder="Password" name="password" type="password" value="">
 							</div>
-							<button type="submit"class="btn btn-primary"><i class="glyphicon glyphicon-log-in"></i>  Login</button>
+							<div class="form-group">
+								<select name="status_pegawai" class="form-control">
+									<option value="admin">Admin</option>
+									<option value="pegawai">Pegawai</option>
+								</select>
+							</div>
+							<div class="form-group">
+						<input type="submit" name="login" class="btn btn-primary btn-block" value="Log me in" />
 						</fieldset>
 					</form>
 				</div>
