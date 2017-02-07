1e87e264f972fd3badd3638c0cef6f19a5a23625
yuaneko95 <yuaneko95@gmail.com>
2017-02-07 15:38:34 +0700

    ape mari

--- proses/tambah_cuti.php
+++ proses/tambah_cuti.php
@@ -11,8 +11,14 @@
 	$status = $_POST['status'];
 	$selisih = (strtotime($tgl_akhir_cuti) - strtotime($tgl_mulai_cuti))/(60*60*24);
 
-	$sql = "INSERT INTO permohonan_cuti VALUES('','$id_pegawai','$id_jcuti','$tgl_pengajuan', '$selisih', '$tgl_mulai_cuti','$tgl_akhir_cuti','$alasan','$status','','')";
-	$s = mysqli_query($conn, $sql) or die (mysqli_error($conn));
-
-	header('location:../ajukan_cuti.php');
+	$sql1 = mysqli_query($conn, "SELECT * FROM permohonan_cuti WHERE id_pegawai = '$id_pegawai'") or die(mysqli_error($conn));
+	$row = mysqli_fetch_row($sql1);
+	$boh = $row['jatah_cuti'] - $selisih ;
+	if ($boh <= '0' ) {
+		echo '<script>Alert("data gagal masuk");</script>';
+	} else {
+		$sql = "INSERT INTO permohonan_cuti VALUES('','$id_pegawai','$id_jcuti','$tgl_pengajuan', '$selisih', '$tgl_mulai_cuti','$tgl_akhir_cuti','$alasan','$status','','')";
+		$s = mysqli_query($conn, $sql) or die (mysqli_error($conn));
+	}
 ?>
+<meta http-equiv="refresh" content="0;URL='../ajukan_cuti.php'" />
