692dd1996cc2587734f5439aa58b6794294bb271
yuaneko95 <yuaneko95@gmail.com>
2017-02-07 12:29:55 +0700

    fix disahkan

--- admin/proses_jon.php
+++ admin/proses_jon.php
@@ -1,16 +1,17 @@
 <?php 
 session_start();
 include 'koneksi.php';
+$username = $_SESSION['username'];
 $id_pcuti = $_POST['id_pcuti'];
 $tgl_sah = date('Y/m/d');
 $id_jcuti = $_POST['id_jcuti'];
 	
 if ($_POST['id_jcuti'] != '2' ) {
-	$sql = "UPDATE permohonan_cuti SET status = 'disetujui', tgl_sah = '$tgl_sah' WHERE id_pcuti = '$id_pcuti'";
+	$sql = "UPDATE permohonan_cuti SET status = 'disetujui', tgl_sah = '$tgl_sah', disahkan = '$username' WHERE id_pcuti = '$id_pcuti'";
 }else{
 	$sql = "UPDATE pegawai
 		INNER JOIN permohonan_cuti ON permohonan_cuti.id_pegawai = pegawai.id_pegawai
-		SET jatah_cuti = jatah_cuti - lama_cuti, STATUS = 'disetujui', tgl_sah = '$tgl_sah' WHERE id_pcuti = '$id_pcuti'";
+		SET jatah_cuti = jatah_cuti - lama_cuti, STATUS = 'disetujui', tgl_sah = '$tgl_sah', disahkan = '$username' WHERE id_pcuti = '$id_pcuti'";
 }
 
 $s = mysqli_query($conn, $sql) or die (mysqli_error($conn));
--- proses/tambah_cuti.php
+++ proses/tambah_cuti.php
@@ -11,7 +11,7 @@
 	$status = $_POST['status'];
 	$selisih = (strtotime($tgl_akhir_cuti) - strtotime($tgl_mulai_cuti))/(60*60*24);
 
-	$sql = "INSERT INTO permohonan_cuti VALUES('','$id_pegawai','$id_jcuti','$tgl_pengajuan', '$selisih', '$tgl_mulai_cuti','$tgl_akhir_cuti','$alasan','$status','')";
+	$sql = "INSERT INTO permohonan_cuti VALUES('','$id_pegawai','$id_jcuti','$tgl_pengajuan', '$selisih', '$tgl_mulai_cuti','$tgl_akhir_cuti','$alasan','$status','','')";
 	$s = mysqli_query($conn, $sql) or die (mysqli_error($conn));
 
 	header('location:../ajukan_cuti.php');
