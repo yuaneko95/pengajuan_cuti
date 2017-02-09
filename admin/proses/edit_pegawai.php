54ea36f64214ea17fb4a02e27694ad1ce36dcf5e
el-fath <rochman003@gmail.com>
2017-02-09 12:51:14 +0700

    tampilan index

--- admin/proses/edit_pegawai.php
+++ admin/proses/edit_pegawai.php
@@ -1,8 +1,8 @@
 <?php 
 	include "../koneksi.php";
-	$option = ['cost' => 10,];
 	$id_pegawai 	= $_POST['id_pegawai'];
 	$nama_pegawai 	= $_POST['nama_pegawai'];
+	$username		= $_POST['username'];
 	$id_jabatan 	= $_POST['id_jabatan'];
 	$jenis_kelamin 	= $_POST['jenis_kelamin'];
 	$email 			= $_POST['email'];
@@ -33,9 +33,9 @@
 	}
 
 	if ($_FILES["foto"]["name"] == ""){
-	$a = "UPDATE pegawai SET id_pegawai = '$id_pegawai', nama_pegawai = '$nama_pegawai', id_jabatan = '$id_jabatan', email = '$email', jenis_kelamin = '$jenis_kelamin', alamat_pegawai = '$alamat_pegawai', telpon_pegawai = '$telpon_pegawai', status_pegawai = '$status_pegawai' WHERE id_pegawai = '$id_pegawai'";
+	$a = "UPDATE pegawai SET id_pegawai = '$id_pegawai', nama_pegawai = '$nama_pegawai', username='$username', id_jabatan = '$id_jabatan', email = '$email', jenis_kelamin = '$jenis_kelamin', alamat_pegawai = '$alamat_pegawai', telpon_pegawai = '$telpon_pegawai', status_pegawai = '$status_pegawai' WHERE id_pegawai = '$id_pegawai'";
 	}else{
-	$a = "UPDATE pegawai SET id_pegawai = '$id_pegawai', nama_pegawai = '$nama_pegawai', id_jabatan = '$id_jabatan', email = '$email', jenis_kelamin = '$jenis_kelamin', alamat_pegawai = '$alamat_pegawai', telpon_pegawai = '$telpon_pegawai', status_pegawai = '$status_pegawai',foto = '$file' WHERE id_pegawai = '$id_pegawai'";
+	$a = "UPDATE pegawai SET id_pegawai = '$id_pegawai', nama_pegawai = '$nama_pegawai',username='$username', id_jabatan = '$id_jabatan', email = '$email', jenis_kelamin = '$jenis_kelamin', alamat_pegawai = '$alamat_pegawai', telpon_pegawai = '$telpon_pegawai', status_pegawai = '$status_pegawai',foto = '$file' WHERE id_pegawai = '$id_pegawai'";
   	}
 	$b = mysqli_query($conn,$a) or die (mysqli_error());
 	if ($b == true){
